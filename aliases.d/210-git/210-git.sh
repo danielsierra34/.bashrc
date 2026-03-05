b8fa81e74d88a31ac8125ceefed88dc675def87d
dani-sie <dani-sie@uniandes.edu.co>
2026-03-05 10:42:10 -0500

    se mueve al archivo vm.sh

--- aliases.d/210-git/210-git.sh
+++ aliases.d/210-git/210-git.sh
@@ -16,20 +16,3 @@ git_name() {
     git config --global user.name "$1"
     echo "Nombre de Git configurado como: $1"
 }
-
-git_push_dev_test() {
-    if [ -z "$1" ]; then
-        echo "Uso: git_push_dev_test \"mensaje de commit\""
-        return 1
-    fi
-    ssh_activar debian-o-ngds || return 1
-    git checkout develop || return 1
-    git add . || return 1
-    git commit -m "$1" || return 1
-    git push || return 1
-    git checkout test || return 1
-    git merge develop --no-edit || return 1
-    git push || return 1
-    git checkout develop || return 1
-}
-
--- aliases.d/800-vm/vm.sh
+++ aliases.d/800-vm/vm.sh
@@ -1,1 +1,17 @@
 alias iadnode_connect='ssh -i vm.pem userngds@172.200.151.176'
+
+git_push_dev_test() {
+    if [ -z "$1" ]; then
+        echo "Uso: git_push_dev_test \"mensaje de commit\""
+        return 1
+    fi
+    ssh_activar debian-o-ngds || return 1
+    git checkout develop || return 1
+    git add . || return 1
+    git commit -m "$1" || return 1
+    git push || return 1
+    git checkout test || return 1
+    git merge develop --no-edit || return 1
+    git push || return 1
+    git checkout develop || return 1
+}
