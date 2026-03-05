1e6defa3bbe9ba3346f1452ce342c78b10e16004
dani-sie <dani-sie@uniandes.edu.co>
2026-03-05 10:41:15 -0500

    ajuste git_push_dev_test

--- aliases.d/210-git/210-git.sh
+++ aliases.d/210-git/210-git.sh
@@ -22,6 +22,7 @@ git_push_dev_test() {
         echo "Uso: git_push_dev_test \"mensaje de commit\""
         return 1
     fi
+    ssh_activar debian-o-ngds || return 1
     git checkout develop || return 1
     git add . || return 1
     git commit -m "$1" || return 1
@@ -31,5 +32,4 @@ git_push_dev_test() {
     git push || return 1
     git checkout develop || return 1
 }
-
 
